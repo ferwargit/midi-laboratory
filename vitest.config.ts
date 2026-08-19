4404ce3e9cf8d269f7551cae8d8185fdde790e03
Fernando Warno <ferwar@hotmail.com>
2026-08-19 07:12:47 -0300

    feat: ajustar configuración de Vitest para mejorar el rendimiento de las pruebas

--- vitest.config.ts
+++ vitest.config.ts
@@ -5,7 +5,10 @@ export default defineConfig({
   test: {
     globals: true,
     environment: 'jsdom',
-    include: ['src/**/*.{test,spec}.ts']
+    include: ['src/**/*.{test,spec}.ts'],
+    pool: 'forks',
+    maxWorkers: 1,
+    fileParallelism: false
   },
   resolve: {
     alias: {
